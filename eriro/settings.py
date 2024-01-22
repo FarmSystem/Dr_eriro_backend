05649dabb7c911215152a731ef21620a8ea49494
천기정 <chun@cheongijeong-ui-MacBookAir.local>
2024-01-22 17:11:32 +0900

    fix: 배포 재수정

--- eriro/settings.py
+++ eriro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fy31xqkv7^!s61)39k2gcasaxu8_x-1o)f#pb8lxlnn+vdxal7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-15-165-145-132.ap-northeast-2.compute.amazonaws.com','15.165.145.132:8000','127.0.0.1']
+ALLOWED_HOSTS = ['ec2-15-165-145-132.ap-northeast-2.compute.amazonaws.com','15.165.145.132:8000','127.0.0.1','15.165.145.132']
 
 
 # Application definition
